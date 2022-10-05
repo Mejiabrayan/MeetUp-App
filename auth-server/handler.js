5f436b63fc1b68499cd18e1ae955cd7ca6f981e6
mejiabrayan <learnwithbrayan@gmail.com>
2022-10-05 16:34:31 -0700

    testing

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -3,9 +3,10 @@ const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
 
 // Create a new OAuth2 client with the configured keys. You will need to set the redirect URI if you have not already done so.
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
 const credentials = {
+
   client_id: process.env.CLIENT_ID,
   project_id: process.env.PROJECT_ID,
   client_secret: process.env.CLIENT_SECRET,
@@ -13,25 +14,27 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://Mejiabrayan.github.io/MeetUp-App/"],
+  redirect_uris: ["https://Mejiabrayan.github.io/MeetUp-App"],
   javascript_origins: ["https://Mejiabrayan.github.io", "http://localhost:3000"],
 };
 
 // Create a new OAuth2 client with the configured keys. You will need to set the redirect URI if you have not already done so.
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
-
 const oAuth2Client = new google.auth.OAuth2(
   client_id,
   client_secret,
   redirect_uris[0]
 );
 
+
 // Generate a consent page url that allows scopes listed above and returns a url that will allow offline access.
 module.exports.getAuthURL = async () => {
+
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
-    scope: SCOPES
-  })
+    scope: SCOPES,
+  });
+
 
 
 
@@ -44,4 +47,4 @@ module.exports.getAuthURL = async () => {
       authUrl: authUrl,
     }),
   };
-};
+}
