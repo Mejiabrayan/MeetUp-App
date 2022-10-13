1f08c0f89a8bbcda8537b706c5b668fc4979f7d4
mejiabrayan <learnwithbrayan@gmail.com>
2022-10-13 16:32:37 -0700

    added api

---
+++ src/api.js
@@ -0,0 +1,14 @@
+// used to store function extractLocations which extracts event locations out of an array of events and remove duplicates
+/**
+ *
+ * @param {*} events:
+ * The following function should be in the “api.js” file.
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+ * The Set will remove all duplicates from the array.
+ */
+ export const extractLocations = (events) => {
+    var extractLocations = events.map((event) => event.location);
+    var locations = [...new Set(extractLocations)];
+    return locations;
+  };
