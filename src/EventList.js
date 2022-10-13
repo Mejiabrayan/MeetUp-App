07a6117c8116cc56b5beb8392a8ecd600de7d092
mejiabrayan <learnwithbrayan@gmail.com>
2022-10-13 16:30:01 -0700

    added eventlist component

---
+++ src/EventList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Event from './Event'; 
+
+class EventList extends Component {
+    // Event prop
+    render() {
+        const { events } = this.props;
+        return (
+            <ul className='EventList'>
+                {events.map(event =>
+                    <li key={event.id}>
+                        {/* Within the list elements there are event components */}
+                        <Event event={event} />
+                    </li>
+                )}
+            </ul >
+        )
+    }
+}
+export default EventList;
