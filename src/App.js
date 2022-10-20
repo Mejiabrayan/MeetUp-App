0057bbe94c6c56a4a87d466a2ab530dbe7844ce6
mejiabrayan <learnwithbrayan@gmail.com>
2022-10-20 16:21:20 -0700

    updated components with mock data

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import {mockData} from './mock-data';
+import {extractLocations} from './api'
 
-export class App extends Component {
-  state = {
-    events: [],
-    locations: []
-  }
+class App extends Component {
+ state = {
+  events: mockData,
+  locations: extractLocations(mockData)
+ }
   render() {
     return (
       <div className="App">
         <h1> MeetUp 📍 </h1>
         <h3> Find events near you </h3>
-        <CitySearch locations={this.state.locations} />
-        <EventList events={this.state.events} />
+        <CitySearch  locations={this.state.locations}/>
         <NumberOfEvents />
+        <EventList events={this.state.events} />
       </div>
     );
   }
