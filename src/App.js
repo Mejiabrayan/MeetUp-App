ab21b9be8a6426e520696a1d3a06eb1a6a29bff3
mejiabrayan <learnwithbrayan@gmail.com>
2022-10-21 00:41:44 -0700

    added handler and props

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import {mockData} from './mock-data';
-import {extractLocations} from './api'
+import { mockData } from './mock-data';
+import { extractLocations } from './api'
 
 class App extends Component {
- state = {
-  events: mockData,
-  locations: extractLocations(mockData)
- }
+  state = {
+    events: mockData,
+    locations: extractLocations(mockData),
+    totalEvents: 32,
+  }
+
+  handleInputChanged = (e) => {
+    const value = e.target.value;
+    this.setState({
+      totalEvents: value
+    })
+  }
   render() {
+    const { events, locations, totalEvents } = this.state;
     return (
       <div className="App">
         <h1> MeetUp 📍 </h1>
         <h3> Find events near you </h3>
-        <CitySearch  locations={this.state.locations}/>
-        <NumberOfEvents />
-        <EventList events={this.state.events} />
+        <CitySearch locations={locations} />
+        <NumberOfEvents totalEvents={totalEvents} handleInputChanged={this.handleInputChanged} />
+        <EventList events={events.slice(0, totalEvents)} />
       </div>
     );
   }
