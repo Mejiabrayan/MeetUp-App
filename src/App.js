96b7cb2cf80139a44315b8b7fc862287abcd16c0
mejiabrayan <learnwithbrayan@gmail.com>
2022-10-23 14:03:36 -0700

    added new changes

--- src/App.js
+++ src/App.js
@@ -13,41 +13,26 @@ class App extends Component {
     totalEvents: 32,
   }
 
-<<<<<<< HEAD
-=======
   // Update events based on city selected by user
->>>>>>> integrationTest
   handleInputChanged = (e) => {
     const value = e.target.value;
     this.setState({
       totalEvents: value
     })
   }
-<<<<<<< HEAD
-  // updates the state of events
-  updateEvents = (location) => {
-=======
 
   // 
   updateEvents = (location, eventCount = this.state.totalEvents) => {
->>>>>>> integrationTest
     getEvents().then((events) => {
       const locationEvents = (location === 'all') ?
         events :
         events.filter((event) => event.location === location);
       this.setState({
-<<<<<<< HEAD
-        events: locationEvents
-=======
         events: locationEvents.slice(0, eventCount)
->>>>>>> integrationTest
       });
     });
   }
 
-<<<<<<< HEAD
-
-=======
   componentDidMount() {
     this.mounted = true;
     getEvents().then((events) => {
@@ -60,7 +45,6 @@ class App extends Component {
   componentWillUnmount() {
     this.mounted = false;
   }
->>>>>>> integrationTest
   render() {
     const { events, locations, totalEvents } = this.state;
     return (
@@ -69,11 +53,7 @@ class App extends Component {
         <h3> Find events near you </h3>
         <CitySearch locations={locations} updateEvents={this.updateEvents} />
         <NumberOfEvents totalEvents={totalEvents} handleInputChanged={this.handleInputChanged} />
-<<<<<<< HEAD
-        <EventList events={events.slice(0, totalEvents)} />
-=======
         <EventList events={events} />
->>>>>>> integrationTest
       </div>
     );
   }
