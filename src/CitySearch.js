4f921f3dbcf0345c5cf5fc372cd8e8cd401a1f09
mejiabrayan <learnwithbrayan@gmail.com>
2022-11-02 19:15:55 -0700

    added infoAlert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alerts';
 
 
 export class CitySearch extends Component {
@@ -6,23 +7,33 @@ export class CitySearch extends Component {
     state = {
         query: '',
         suggestions: [],
-        showSuggestions: undefined
+        showSuggestions: undefined,
+        infoText: ''
     }
     handleInputChanged = (event) => {
         const value = event.target.value;
         const suggestions = this.props.locations.filter((location) => {
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
-        this.setState({
-            query: value,
-            suggestions,
-        });
-        console.log(suggestions)
-    };
+
+        if (suggestions.length === 0) {
+            this.setState({
+                query: value,
+                infoText: 'Not found, try again '
+            });
+        } else {
+            return this.setState({
+                query: value,
+                suggestions,
+                infoText: ''
+            })
+        }
+    }
     handleItemClicked = (suggestion) => {
         this.setState({
             query: suggestion,
-            showSuggestions: false
+            showSuggestions: false,
+            infoText: ''
         });
         this.props.updateEvents(suggestion);
     }
@@ -31,6 +42,7 @@ export class CitySearch extends Component {
     render() {
         return (
             <div className='CitySearch'>
+                <InfoAlert text={this.state.infoText} />
                 <input
                     placeholder='Search for city'
                     type='text'
