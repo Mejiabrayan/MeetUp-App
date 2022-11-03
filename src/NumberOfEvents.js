db9cafdd06bf6339d5f3784e75e802aea94e30eb
mejiabrayan <learnwithbrayan@gmail.com>
2022-11-02 19:15:14 -0700

    imported warningalert and erroralert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { ErrorAlert, WarningAlert } from './Alerts';
 
 // The code is a component that displays the number of events in an input field.
 class NumberOfEvents extends Component {
 
     render() {
-        const { totalEvents, handleInputChanged } = this.props
+        const { totalEvents, handleInputChanged, errorText, warningText } = this.props
         return (
             <div className='numberOfEvents'>
                 <label>
@@ -16,6 +17,8 @@ class NumberOfEvents extends Component {
                         value={totalEvents}
                         onChange={handleInputChanged}
                     />
+                      <ErrorAlert text={errorText} />
+                <WarningAlert text={warningText} />
                 </label>
             </div>
         )
