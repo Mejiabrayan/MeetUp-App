734ba1903139f3bea644ee31a18744af8b2dba2e
mejiabrayan <learnwithbrayan@gmail.com>
2023-02-01 11:26:06 -0800

    refactoring class component to function component, set state of lists to 5

---
+++ src/EventList.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Event from './components/Event';
+
+export default function EventList({ events }) {
+  const [visibleEvents, setVisibleEvents] = useState(events.slice(0, 5));
+  const [showAll, setShowAll] = useState(false);
+
+  const toggleShowAll = () => {
+    setShowAll(!showAll);
+    setVisibleEvents(showAll ? events.slice(0, 5) : events);
+  };
+
+  return (
+    <div>
+      {visibleEvents.map((event) => (
+        <Event key={event.id} event={event} />
+      ))}
+      {events.length > 5 && (
+        <button onClick={toggleShowAll}>
+          {showAll ? 'Show Less' : 'See More Events'}
+        </button>
+      )}
+    </div>
+  );
+
+}
